35f5c33f71ac5c260796cd0fbdab8a9878786b59
Mercedes Coyle <me@mercedescoyle.com>
2018-10-18 19:33:56 +0000

    Moar installing things

--- shell_setup.sh
+++ shell_setup.sh
@@ -8,13 +8,15 @@ install () {
 	
     if [ "$output" = "" ]; then
 		echo "$program not found - installing"
-		sudo apt-get install $program
+		sudo apt-get -y install $program
 	else
 		echo "$program already installed - skipping"
 	fi
 }
 
 install tmux
+install direnv
+install arduino
 
 link_dir_path=$(pwd)
 
@@ -24,6 +26,15 @@ if [ -e ~/.vim/autoload/plug.vim ]; then
 else
   echo "installing vim-plug"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# install go
+if [ -e /usr/local/go ]; then
+  echo "go already installed - skipping"
+else
+  echo "installing go v1.11"
+  curl -Lo go1.11.1.linux-amd64.tar.gz https://golang.org/doc/install?download=go1.11.1.linux-amd64.tar.gz
+  tar -xvf go1.11.1.linux-amd64.tar.gz -C /usr/local && rm go1.11.1.linux-amd64.tar.gz
 fi
 
 # symlink config files 
@@ -41,3 +52,9 @@ else
 	ln -s "$link_dir_path/.gitconfig" ~/.gitconfig
 fi
 
+if [ -e ~/.bash_profile ]; then
+	echo ".bash_profile already exists - skipping"
+else
+	echo "symlinking ~/.bash_profile from $link_dir_path/.bash_profile"
+	ln -s "$link_dir_path/.bash_profile" ~/.bash_profile
+fi
