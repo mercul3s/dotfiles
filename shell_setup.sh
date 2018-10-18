2123ce2db868c70efbe2dabeb44e7692ae782d11
Mercedes Coyle <me@mercedescoyle.com>
2018-10-18 18:39:25 +0000

    Added vim-plug install

--- shell_setup.sh
+++ shell_setup.sh
@@ -5,8 +5,8 @@ echo "starting dev environment configuration"
 install () {
 	local program=$1
 	local output=$(command -v $program)
-	echo $output
-	if [ "$output" = "" ]; then
+	
+    if [ "$output" = "" ]; then
 		echo "$program not found - installing"
 		sudo apt-get install $program
 	else
@@ -17,6 +17,14 @@ install () {
 install tmux
 
 link_dir_path=$(pwd)
+
+# install vim plug
+if [ -e ~/.vim/autoload/plug.vim ]; then
+  echo "vim-plug already installed - skipping"
+else
+  echo "installing vim-plug"
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # symlink config files 
 if [ -e ~/.vimrc ]; then
